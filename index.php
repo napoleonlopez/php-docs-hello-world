e40c1e33abec47c81e6dfa027ddfa9edff1f89d7
napoleonlopez <74079958+napoleonlopez@users.noreply.github.com>
2023-11-04 20:14:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 <!DOCTYPE html>
 <html>
-<body style="background-color: <?php echo '#0000FF'; ?>;">
+<body style="background-color: <?php echo '#1F3C8C'; ?>;">
 
-<h1>Administración de Servicios en la Nube.</h1>
+<h1 style="color: <?php echo '#FFFFFF'; ?>;">Administración de Servicios en la Nube</h1>
 
 
 </body>
